8b9e3d569b3a1465b2e4eaefe35f7763b142e1a8
Simon Erhardt <hello@rootlogin.ch>
2019-01-27 20:27:22 +0100

    new deploy mechanism

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,10 @@
 set -e
 
 export REZEPTVERWALTUNG_VERSION="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)"
+export SUBFOLDER="latest-$(date +%Y-%m-%d)"
 
 echo "Uploading jar..."
-curl -T "target/rezeptverwaltung-$REZEPTVERWALTUNG_VERSION.jar" -u "$BINTRAY_USER:$BINTRAY_API_KEY" -H "X-Bintray-Publish:1" -H "X-Bintray-Package:linux" -H "X-Bintray-Version:latest-$(date +%Y-%m-%d)" -H "X-Bintray-Override:1" "https://api.bintray.com/content/rootlogin/rezeptverwaltung/latest-$(date +%Y-%m-%d)/rezeptverwaltung.jar"
+curl -T "target/rezeptverwaltung-$REZEPTVERWALTUNG_VERSION.jar" -u "$BINTRAY_USER:$BINTRAY_API_KEY" -H "X-Bintray-Publish:1" -H "X-Bintray-Package:rezeptverwaltung" -H "X-Bintray-Version:latest-$(date +%Y-%m-%d)" -H "X-Bintray-Override:1" "https://api.bintray.com/content/rootlogin/generic/$SUBFOLDER/rezeptverwaltung.jar"
 
 echo "Uploading exe..."
-curl -T "target/rezeptverwaltung-$REZEPTVERWALTUNG_VERSION.exe" -u "$BINTRAY_USER:$BINTRAY_API_KEY" -H "X-Bintray-Publish:1" -H "X-Bintray-Package:windows" -H "X-Bintray-Version:latest-$(date +%Y-%m-%d)" -H "X-Bintray-Override:1" "https://api.bintray.com/content/rootlogin/rezeptverwaltung/latest-$(date +%Y-%m-%d)/rezeptverwaltung.exe"
+curl -T "target/rezeptverwaltung-$REZEPTVERWALTUNG_VERSION.exe" -u "$BINTRAY_USER:$BINTRAY_API_KEY" -H "X-Bintray-Publish:1" -H "X-Bintray-Package:rezeptverwaltung" -H "X-Bintray-Version:latest-$(date +%Y-%m-%d)" -H "X-Bintray-Override:1" "https://api.bintray.com/content/rootlogin/generic/$SUBFOLDER/rezeptverwaltung.exe"
